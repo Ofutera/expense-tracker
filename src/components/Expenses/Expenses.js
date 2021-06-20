2129932ce3b5e5883351db44105da7bbc4ac0c9b
Ondrej Futera <ondra.futera@gmail.com>
2021-06-20 12:36:08 +0200

    Add state to exp array to render exp dynamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,37 +5,25 @@ import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
-
-  const [year, setYear] = useState('2021')
+  const [year, setYear] = useState("2021");
 
   const selectedYearHandler = (selectedYear) => {
     setYear(selectedYear);
   };
 
-
   return (
     <Card className="expenses">
       <ExpenseFilter selected={year} onSelectedYear={selectedYearHandler} />
-      <ExpenseItem
-        title={props.expArray[0].title}
-        amount={props.expArray[0].amount}
-        date={props.expArray[0].date}
-      />
-      <ExpenseItem
-        title={props.expArray[1].title}
-        amount={props.expArray[1].amount}
-        date={props.expArray[1].date}
-      />
-      <ExpenseItem
-        title={props.expArray[2].title}
-        amount={props.expArray[2].amount}
-        date={props.expArray[2].date}
-      />
-      <ExpenseItem
-        title={props.expArray[3].title}
-        amount={props.expArray[3].amount}
-        date={props.expArray[3].date}
-      />
+      {props.expArray.map((expense) => {
+        return (
+          <ExpenseItem
+            key = {expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
     </Card>
   );
 };
